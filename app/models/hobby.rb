6a09360bac5cc2cdfe5ac275c67ed27bf3c28c98
stephany ramirez <stephany_re@hotmail.com>
2022-03-01 09:10:28 -0700

    center sign up

--- app/models/hobby.rb
+++ app/models/hobby.rb
@@ -1,6 +1,6 @@
 class Hobby < ApplicationRecord
   has_one_attached :photo
-  belongs_to :category, through: :category_hobby
+  has_many :categories, through: :category_hobby
   validates :name, presence: true
   validates :description, presence: true
 end
