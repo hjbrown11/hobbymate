3a69e97ff7ae23e7ecc7d37858b2c4c837d40fce
Astrid de Jaegher <astriddejaegher97@gmail.com>
2022-03-01 12:58:38 +0100

    db

--- app/models/hobby.rb
+++ app/models/hobby.rb
@@ -1,5 +1,5 @@
 class Hobby < ApplicationRecord
-  belongs_to :category
+  belongs_to :category, through: :category_hobby
 
   validates :name, presence: true
   validates :description, presence: true
