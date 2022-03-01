437520fe477d116299c0d12abe9031729222baa6
Hannah Brown <hannah.j.brown@hotmail.co.uk>
2022-03-01 15:01:56 +0100

    Homepage form and controller logic

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,6 +3,21 @@ class CategoriesController < ApplicationController
 
   def index
     @categories = Category.all
+    if params[:categories]
+      ids = params[:categories].keys.select do |id|
+        params[:categories][id] == "1"
+      end
+      @categories = Category.where(id: ids)
+    end
+
+    if params[:address]
+      current_user.address = params[:address]
+      current_user.save
+    end
+
+    if current_user.address.blank?
+      redirect_to root_path, alert: "Please enter a location to continue"
+    end
   end
 
   def show
