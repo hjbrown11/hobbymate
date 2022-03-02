068431135247ae9d93e5eac94117bac4c928ceb6
Hannah Brown <hannah.j.brown@hotmail.co.uk>
2022-03-02 16:31:19 +0100

    Changes to routes and added match pages

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,2 +1,11 @@
 class MatchesController < ApplicationController
+  def index
+    @matches = Match.where("sender_id = #{current_user.id} OR receiver_id = #{current_user.id}")
+  end
+
+  def show
+  end
+
+  def destroy
+  end
 end
