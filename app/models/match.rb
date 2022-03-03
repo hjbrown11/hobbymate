ed68bed6533d3f68722634a02bf6853d54ae0c74
stephany ramirez <stephany_re@hotmail.com>
2022-03-03 08:18:36 -0700

    change the type of match to integer

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,4 +1,10 @@
 class Match < ApplicationRecord
   belongs_to :sender, class_name: "User"
   belongs_to :receiver, class_name: "User"
+
+  enum match: {
+    pending: 0,
+    accepted: 1,
+    rejected: 2
+  }
 end
