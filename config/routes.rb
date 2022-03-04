1f93b8f0bb7a2120c169b5a351b0a393d5f9ea3c
Hannah Brown <hannah.j.brown@hotmail.co.uk>
2022-03-04 14:12:40 +0100

    messaging working

--- config/routes.rb
+++ config/routes.rb
@@ -2,24 +2,20 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :users, only: [:index, :show, :destroy, :edit, :update]
-
+  resources :users, only: %i[index show destroy edit update]
 
   get 'new_match', to: 'matches#new_match', as: :new_match
 
-
   resources :categories, only: [:index] do
-    resources :hobbies, only: [:index, :show]
+    resources :hobbies, only: %i[index show]
   end
   resources :matches, except: :edit do
-    resources :messages, only: [:create, :index]
+    resources :messages, only: %i[create]
   end
 
-
-  resources :user_hobbies, only: [:create, :new, :destroy] do
+  resources :user_hobbies, only: %i[create new destroy] do
     collection do
       post "create_many"
     end
   end
-
 end
