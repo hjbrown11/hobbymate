18914d0b2c3f329e17616d425589b25aef64a066
Hannah Brown <hannah.j.brown@hotmail.co.uk>
2022-03-07 15:54:18 +0100

    Small change to redirection logic

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    if current_user.address.nil? == false
+    if current_user.address.present?
       redirect_to new_match_path
     else
       @categories = Category.all
