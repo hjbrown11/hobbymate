29d3ac15740f7414e1ec4803391f85b918e07b1b
stephany ramirez <stephany_re@hotmail.com>
2022-04-04 18:36:03 -0700

    homepage doesnt need log in

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,13 @@
 class PagesController < ApplicationController
-  before_action :authenticate_user!
+  skip_before_action :authenticate_user!
   def home
-    if current_user.address.present?
-      redirect_to new_match_path
-    else
+    if current_user.nil?
       @categories = Category.all
       @user = current_user
+    else
+      if current_user.address.present?
+        redirect_to new_match_path
+      end
     end
   end
 end
